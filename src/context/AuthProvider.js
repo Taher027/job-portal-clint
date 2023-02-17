41a9dc74dec6b7328f4a40d5740f0a4f1c15e905
Taher027 <abutaherh27@gmail.com>
2023-02-17 16:20:20 +0600

    create project set firebase react router set router create layout page

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,61 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../Firebase/firebase.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginWithGoogle = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const logOut = () => {
+    setLoading(false);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    createUser,
+    signIn,
+    loginWithGoogle,
+    logOut,
+    user,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ src/pages/Shared/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-blue-500">this is header</h1>
+    </div>
+  );
+};
+
+export default Header;
